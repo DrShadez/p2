b6c4cd4adbaf22b33639e72bfb6c0e8473a62fed
yashsachdev1 <70424132+yashsachdev1@users.noreply.github.com>
2022-07-26 16:06:12 -0700

    update insert

--- p2.sql
+++ p2.sql
@@ -14,3 +14,14 @@ CREATE TABLE IF NOT EXISTS `bushes`(
     `img` varchar(255),
     PRIMARY KEY (`id`)
 );
+
+INSERT INTO bushes
+(`design`, `img`)
+
+VALUES
+('donut bush', ''),
+('spiral bush', ''),
+('maze bush', ''),
+('sphere bush', ''),
+('heart bush', ''),
+('tall bush', '');
