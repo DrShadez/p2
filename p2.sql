09feee2ef07c4f01a32f39f8d8eaa4ed0d950335
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-26 16:18:28 -0700

    Update p2.sql

--- p2.sql
+++ p2.sql
@@ -13,15 +13,16 @@ CREATE TABLE IF NOT EXISTS `bushes`(
     `design` varchar(64),
     `img` varchar(255),
     PRIMARY KEY (`id`)
+
 );
 
-INSERT INTO bushes
+INSERT INTO `bushes`
 (`design`, `img`)
 
 VALUES
-('donut bush', ''),
-('spiral bush', ''),
-('maze bush', ''),
-('sphere bush', ''),
-('heart bush', ''),
-('tall bush', '');
+('donutbush', 'https://backyardgadget.com/wp-content/uploads/2018/12/round-bushes-in-rock-garden-1024x597.jpg'),
+('spiralbush', 'https://www.homestratosphere.com/wp-content/uploads/PhenomHome/Images/Topiary/121689387.jpg'),
+('mazebush', 'https://images-production.gardenvisit.com/uploads/images/15911/glendurgan_garden_118_jpg_original.jpg'),
+('elephantbush', 'https://plantcaretoday.com/wp-content/uploads/topiary-art-elephants-150-t1-min-1024x538.jpeg'),
+('heartbush', 'https://thumbs.dreamstime.com/b/green-bush-heart-shape-standing-line-nature-backgrond-54817509.jpg'),
+('tallbush', 'https://www.thespruce.com/thmb/qB_EZAyyGuCLmGecx5yf4BJ3zpc=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/thujas-515317612-266bd8cf3f42419eb0718a2373215260.jpg');
