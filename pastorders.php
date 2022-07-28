5dc10ab719e697d03e8e96d1e9a4148d2b8317bd
SidShastri <109056979+SidShastri@users.noreply.github.com>
2022-07-28 11:10:25 -0700

    Update pastorders.php

--- pastorders.php
+++ pastorders.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <?php
+//accessing session info
 session_start();
 require 'config.php';
+
+//if user isn't logged in redirect to login
 if(!isset($_SESSION["valid"])){
   header('Location:signin.php');
 }
 try {
+  //query for accessing all of the user's past orders
   $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
   $sth = $dbh -> prepare('SELECT * FROM `pastorders` WHERE `user`=:sessionid');
   $sth->bindValue(":sessionid", $_SESSION["user"]);
@@ -21,6 +25,7 @@ catch (PDOException $e){
 <head>
       <link rel="stylesheet" href="p2.css">
 </head>
+  <!--nav config-->
 <body>
   <ul class="nav">
     <li><a href="home.php">Home </a></li>
@@ -32,6 +37,7 @@ catch (PDOException $e){
 
   <h3>You have previously ordered...</h3>
   <?php
+  //displaying each order
   foreach ($orders as $order) {
 
     echo "A/an " . $order['design'] . " cut which costed $" . $order['cost'];
