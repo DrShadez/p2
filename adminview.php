977e5ef9dddf8fdf15ac28ba1dcfa582d6bc7104
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-28 09:54:52 -0700

    Update adminview.php

--- adminview.php
+++ adminview.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+</head>
+<body class = 'haha'>
+<ul class="nav">
+  <li><a href="home.php">Home </a></li>
+  <li><a href="catalogue.php">Cuts</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="about.php">About</a></li>
+  <li class="logoish"><a  href="home.php">TheCuts</a></li>
+</ul>
 <?php
 session_start();
 if(!isset($_SESSION["adminvalid"])){
@@ -10,3 +21,5 @@ echo "<br>";
 echo "<a href='logout.php'>logout</a>";
 
 ?>
+</body>
+</html>
