09893bc6ece277d19204ed2f5bfa7759978d6147
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-27 20:39:56 -0700

    asdfasf

--- cart.php
+++ cart.php
@@ -1,25 +1,104 @@
+<!DOCTYPE html>
 <?php
 session_start();
 require 'config.php';
-
+if(!isset($_SESSION["valid"])){
+  header('Location:https://atdpsites.berkeley.edu/rmaji/p2/signin.php');
+}
 try {
   $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-  $sth = $dbh -> prepare('SELECT * FROM `current_order`');
+  $sth = $dbh -> prepare('SELECT * FROM `bushes`');
   $sth -> execute();
-  $currentbush = $sth -> fetchAll();
-
-
-  $design=$_POST['bushoption'];
-  var_dump($design);
-  $img=2;
-  $sth1 = $dbh -> prepare('INSERT INTO `current_order` (design,img) VALUES(:design,:img) ');
-  $sth1->bindValue(":design",$design);
-    $sth1->bindValue(":img",$img);
+  $bush = $sth -> fetchAll();
 }
 catch (PDOException $e){
   echo "<p>Error: {$e->getMessage()}</p>";
 }
+$sphere = $_POST['sphere'];
+echo "water";
+echo empty($_POST['sphere']);
+if (empty($_POST['sphere'])) {
+  echo "hesus";
 
+}
+else {
+  $sth = $dbh -> prepare(
+  "INSERT INTO `current_order`
+  (`design`, `img`)
 
+  VALUES('sphere', 'linglong.jpg')");
+  $sth -> execute();
+  $asdf = $sth -> fetchAll();
+  echo "yes";
 
+}
+$spiral = $_POST['spiral'];
+if (empty($_POST['spiral'])) {
+
+}
+else {
+  $sth = $dbh -> prepare(
+  "INSERT INTO current_order
+  (`design`, `img`)
+
+  VALUES('spiral', 'watersa.jpg')");
+  $sth -> execute();
+}
+
+$maze = $_POST['maze'];
+if (empty($_POST['maze'])) {
+
+}
+else {
+
+  $sth = $dbh -> prepare(
+  "INSERT INTO `current_order`
+  (`design`, `img`)
+
+  VALUES('maze', 'mazebsuh.jpg')");
+  $sth -> execute();
+}
+  $elephant = $_POST['elephant'];
+if (empty($_POST['elephant'])) {
+
+}
+else {
+  $sth = $dbh -> prepare(
+  "INSERT INTO `current_order`
+  (`design`, `img`)
+
+  VALUES('maze', 'mazebsuh.jpg')");
+  $sth -> execute();
+}
+  $heart = $_POST['heart'];
+if (empty($_POST['heart'])) {
+
+}
+else {
+
+}
+  $tall = $_POST['tall'];
+if (empty($_POST['tall'])) {
+
+}
+else {
+
+}
+try {
+  $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+  $sth = $dbh -> prepare('SELECT * FROM `current_order`');
+  $sth -> execute();
+  $orders = $sth -> fetchAll();
+}
+catch (PDOException $e){
+  echo "<p>Error: {$e->getMessage()}</p>";
+}
+var_dump($orders);
 ?>
+<html>
+<head>
+      <link rel="stylesheet" href="p2.css">
+</head>
+<body>
+</body>
+</html>
