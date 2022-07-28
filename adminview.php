e9fdb7f38e59da06628424c062ae85c2ef8f1d59
SidShastri <109056979+SidShastri@users.noreply.github.com>
2022-07-28 10:36:04 -0700

    Update adminview.php

--- adminview.php
+++ adminview.php
@@ -1,7 +1,9 @@
+<!--Basically the main home page for admin-->
 <html>
 <head>
 </head>
 <body class = 'haha'>
+ <!--navbar config-->
 <ul class="nav">
   <li><a href="home.php">Home </a></li>
   <li><a href="catalogue.php">Cuts</a></li>
@@ -10,7 +12,9 @@
   <li class="logoish"><a  href="home.php">TheCuts</a></li>
 </ul>
 <?php
+ //accessing session info
 session_start();
+ //if the user is not an admin, redirect them to signin.php
 if(!isset($_SESSION["adminvalid"])){
   header('Location:signin.php');
 }
