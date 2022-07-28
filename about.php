c3b79396babcf624c219a3290f2f1087eca13cc1
yashsachdev1 <70424132+yashsachdev1@users.noreply.github.com>
2022-07-28 00:04:59 -0700

    about page

---
+++ about.php
@@ -0,0 +1,42 @@
+<html>
+<head>    <link rel="stylesheet" href="p2.css">
+
+</head>
+<body class = 'haha2'>
+
+
+
+<ul class="nav">
+  <li><a href="home.php">Home </a></li>
+  <li><a href="catalogue.php">Cuts</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="about.php">About</a></li>
+  <li class="logoish"><a  href="home.php">TheCuts</a></li>
+</ul>
+<div class = 'center'>
+<p class = 'title'> Contact Us </p>
+</div>
+<table class = 'contact'><tr><td>
+<h1 class="center"> We are always here 24/7 </h1>
+<ol class = 'center'>
+  <li class="size">
+    <p> TheCuts is a Bush Selling Business. </p>
+    <div> Our Bushes are top tier and cannot be beaten.</div>
+  </li>
+  <li class="size">
+    <p> We are a three man company and base our business off of sincerity and honesty</p>
+  </li>
+  <li class="size">
+    <p> Our policy for missing shipments is that you will just have to buy another one. </p>
+    <div> If you complain you're just broke.</div>
+  </li>
+  <li class="size">
+    <p> While paying for your bush, you must give us your SSN and Bank Account information </p>
+    <div> We are very trusted people. DO NOT WORRY!</div>
+  </li>
+</ol>
+</td></tr></table>
+<p class = 'center'>Sign up to access our webstore. Give us your money: <a href = 'signup.php' class = 'button'> SIGN UP </a></p>
+
+</body>
+</html>
