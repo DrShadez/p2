443c64cbac2be68b19b8c10c980f43987c59bd7b
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-28 10:05:48 -0700

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -45,7 +45,7 @@ try {
 
         if($_POST['username']==$adminuser){
           if(password_verify($_POST['pass'],$realpass)){
-            header('Location:https://atdpsites.berkeley.edu/skshastri/AIC/p2/adminview.php');
+            header('Location:adminview.php');
             $_SESSION["adminvalid"]="valid";
         }
         else{
@@ -58,7 +58,7 @@ try {
 
     elseif (isset($userinfo)){
           if(password_verify($_POST['pass'],$realpass)){
-            header('Location:https://atdpsites.berkeley.edu/skshastri/AIC/p2/catalogue.php');
+            header('Location:catalogue.php');
             $_SESSION["valid"]="valid";
             echo "everything worked";
           }
