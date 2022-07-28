8b35bf61fc5d7528d38e1282f29cabbda33bbed3
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-28 03:31:43 -0700

    sdf

--- catalogue.php
+++ catalogue.php
@@ -22,10 +22,11 @@ catch (PDOException $e){
 <body>
 
 <ul class="nav">
-  <li><a href="home.php">Home </a></li>
-  <li><a class="current" href="catalogue.php">Cuts</a></li>
-  <li><a href="#">Contact</a></li>
-  <li><a href="#">About</a></li>
+ <li><a href="home.php">Home </a></li>
+  <li><a href="catalogue.php">Cuts</a></li>
+  <li><a href="cart.php">Cart</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="about.php">About</a></li>
   <li class="logoish"><a  href="home.php">TheCuts</a></li>
 </ul>
 
