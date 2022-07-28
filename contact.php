cbb162a1bdebb8f7b825b3903075e86c43dce71e
yashsachdev1 <70424132+yashsachdev1@users.noreply.github.com>
2022-07-28 00:05:34 -0700

    contact page

---
+++ contact.php
@@ -0,0 +1,42 @@
+<html>
+<head>    <link rel="stylesheet" href="p2.css">
+
+</head>
+<body class = 'haha2'>
+
+
+
+<ul class="nav">
+  <li><a href="home.php">Home </a></li>
+  <li><a href="catalogue.php">Cuts</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="about.php">About</a></li>
+  <li class="logoish"><a  href="home.php">TheCuts</a></li>
+</ul>
+<div class = 'center'>
+<p class = 'title'> Contact Us </p>
+</div>
+<table class = 'contact'><tr><td>
+<h1 class="center"> We are always here 24/7 </h1>
+<ol class = 'center'>
+  <li class="size">
+    <p> If you need to reach out to us, we have 43.75 agents on standby eagerly waiting for you. </p>
+    <div> You can call us at +1 420-555-6969.</div>
+  </li>
+  <li class="size">
+    <p> If you don't have a phone for some reason (loser), then you can email us at urgettingscammed@gmail.com.</p>
+  </li>
+  <li class="size">
+    <p> Please do not call us about a missing shipment because we do not care. </p>
+    <div> Just buy another one.</div>
+  </li>
+  <li class="size">
+    <p> If you buy more than 3 of our products, contact us and we will give you a free water bottle. </p>
+    <div> DISCLAIMER: MUST PROVIDE SSN AND BANK ACCOUNT INFO!!!!</div>
+  </li>
+</ol>
+</td></tr></table>
+<p class = 'center'>Sign up to access our webstore. Click Here: <a href = 'signup.php' class = 'button'> SIGN UP </a></p>
+
+</body>
+</html>
