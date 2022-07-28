8113be0117f73333a11c13bb76911827ab8d1c55
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-28 02:58:23 -0700

    Update p2.sql

--- p2.sql
+++ p2.sql
@@ -28,11 +28,13 @@ VALUES
 ('tall', 'long.jpg');
 
 
+
 CREATE TABLE IF NOT EXISTS `current_order`(
     `id` int NOT NULL AUTO_INCREMENT,
     `design` varchar(64),
     `img` varchar(255),
     `user` varchar(255),
+    `cost` int(12),
     PRIMARY KEY (`id`)
 
 );
