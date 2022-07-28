de21c0f3971f5a4d56eddb005509b5902ec9282b
SidShastri <109056979+SidShastri@users.noreply.github.com>
2022-07-28 10:34:07 -0700

    Update adminuserinfo.php

--- adminuserinfo.php
+++ adminuserinfo.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 </head>
+ //navbar config
 <body class = 'haha'>
 <ul class="nav">
   <li><a href="home.php">Home </a></li>
@@ -12,35 +13,42 @@
 <body>
 
 <?php
+  
+//session starting
 session_start();
 require 'config.php';
+  
+//if the user isn't an admin, relocate to signin
 if(!isset($_SESSION["adminvalid"])){
   header('Location:signin.php');
 }
+//if the user has have a sessionid, proceed
 if(isset($_SESSION["sessionid"])){
 
 }
+//if user doesn't have a sessionid, give them one
 else{
   $_SESSION["sessionid"]= $_POST['renameindividual'];
 }
 try {
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-    //getting multiple row
 
 
+//accessing all the information in user_info table
   $sth = $dbh->prepare("SELECT * FROM user_info");
   $sth->execute();
   $displayinfo=$sth->fetchAll();
 
-
+//form for deleting user
   echo "<form action='updateuser.php' id='update' method='POST'>";
   echo "<select id='updateindividual' name='updateindividual'>";
-
+  
+  //foreach row in the table, if the user is an admin, don't show them in the dropdown
   foreach($displayinfo as $info){
     // var_dump($info);
     $divid=$info['id'];
     // var_dump($divid);
-
+  
       if($info['username']=='admin'){
 
       }
@@ -56,10 +64,11 @@ echo "</form>";
 
 
 
-
+//form for renaming user, same dropdown code
 echo "<form action='updateuser.php' id='update' method='POST'>";
 echo "<select id='renameindividual' name='renameindividual'>";
 
+  
 foreach($displayinfo as $info){
   // var_dump($info);
   $divid=$info['id'];
@@ -75,11 +84,12 @@ foreach($displayinfo as $info){
 echo "</select>";
 echo "<input type='text' name='renamevalue'>";
 
+ 
 echo "<input type='submit' name='renamebtn' id='renamebtn' value='rename '>";
 echo "</form>";
 
 
-
+//form for adding user, same dropdown code
 echo "<form action='updateuser.php' id='add' method='POST'>";
 
 echo "<input type='text' name='adduservalue'>";
