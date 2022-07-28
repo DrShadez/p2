25d02547433002b748b1002a2053e6bde1d8f0a0
yashsachdev1 <70424132+yashsachdev1@users.noreply.github.com>
2022-07-27 23:11:38 -0700

    background updated

--- home.php
+++ home.php
@@ -7,6 +7,14 @@
 
   </head>
 
+  <ul class="nav">
+    <li><a href="home.php">Home </a></li>
+    <li><a href="catalogue.php">Cuts</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">About</a></li>
+    <li class="logoish"><a  href="home.php">TheCuts</a></li>
+  </ul>
+
 <body style = "margin:0">
 
 
@@ -18,13 +26,14 @@
 <!--
 <a class = 'button' href='signin.php'>log in </a>
 <a class = 'button' href='signup.php'>sign up </a> -->
-<div class = 'title'> Welcome to BUSH LAND </div>
-
+<div class = 'title'> Enjoy Our Variety of Exotic Bushes </div>
+<img src = "leave.jpg" alt = 'bushes' class="center2">
+<div>
 <a class = 'button' href='signin.php'>log in </a>
 <a class = 'button' href='signup.php'>sign up </a>
 <a class = 'button' href='catalogue.php'>catalogue </a>
 </div>
-
+<p> Take a look at our catalogue now! </p>
 <div class="parallax2"></div>
 
 </body>
