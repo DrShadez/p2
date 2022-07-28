3719f30521b052ccafe2a58b169b6e92b1bb77c8
SidShastri <109056979+SidShastri@users.noreply.github.com>
2022-07-28 11:04:29 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,17 @@
 <?php
+//accessing session info
 session_start();
 
+
+//if the user isn't logged in, redirect to login
 if(!isset($_SESSION["valid"])){
   header('Location:signin.php');
 }
 
+//destroy the session
 session_destroy();
 
+
+//redirect to home.php
 header('Location:home.php');
 ?>
