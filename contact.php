d414cdf55aeea5cd559485a553fe12f99bf12339
DrShadez <91856092+DrShadez@users.noreply.github.com>
2022-07-28 08:46:30 -0700

    changed

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ session_start();
   <li><a href="home.php">Home </a></li>
   <li><a href="catalogue.php">Cuts</a></li>
   <li><a href="cart.php">Cart</a></li>
-  <li><a href="contact.php">Contact</a></li>
+  <li><a class="current" href="contact.php">Contact</a></li>
   <li><a href="about.php">About</a></li>
   <li class="logoish"><a  href="home.php">TheCuts</a></li>
 </ul>
