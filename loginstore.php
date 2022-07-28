a1aac150fcad2379fd022adf5c952f0c8611fc13
SidShastri <109056979+SidShastri@users.noreply.github.com>
2022-07-28 11:03:33 -0700

    Update loginstore.php

--- loginstore.php
+++ loginstore.php
@@ -1,11 +1,16 @@
 <body>
 
 <?php
+    //accessing session info
 session_start();
 require 'config.php';
+  
+ //if user isn't logged in, redirect to signin
 if(!isset($_SESSION["valid"])){
   header('Location:signin.php');
 }
+  
+  //backend validation for if any of the text fields are empty
 if(empty(htmlspecialchars($_POST['usernamemake']))){
   echo "empty username";
   exit;
@@ -24,23 +29,25 @@ try {
 
   //dbh configuration
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-
+//hashing the password that the users input
     $newpass=password_hash(htmlspecialchars($_POST['passmake']),PASSWORD_DEFAULT);
-
+//storing the username that the users input
     $newusername=htmlspecialchars($_POST['usernamemake']);
 
 
 
-
+//seperate query for getting info from user_info where the user info belongs to the username that is logged in
     $sth = $dbh->prepare("SELECT * FROM user_info WHERE username=:newuser");
     $sth->bindValue(":newuser",$newusername);
     $sth->execute();
     $count=$sth->fetchColumn();
+  //if the username already exists, don't allow it
     if ($count>1){
       echo "username already taken";
     }
 
 
+  //more backend validation
 
     elseif(empty(htmlspecialchars($_POST['usernamemake']))){
       echo "please put a username";
@@ -57,16 +64,18 @@ try {
     elseif(htmlspecialchars($_POST['passmake'])!=htmlspecialchars($_POST['confirmpass'])){
       echo "passwords don't match";
     }
-
+//if the username is admin, give it the value$isadmin=1
     else{
       if (htmlspecialchars($_POST['usernamemake']=='admin')){
         $isadmin=1;
       }
+ //elseif the username isn't admin, don't give them admin powers
       elseif(htmlspecialchars($_POST['usernamemake'])!=='admin'){
 
         $isadmin=0;
       }
 
+  //insert all this information into user_info
         $sth2 = $dbh->prepare("INSERT INTO `user_info` (is_admin, passhash, username) VALUES (:is_admin,:passhash,:username)");
         $sth2->bindValue(":is_admin",$isadmin);
         $sth2->bindValue(":passhash",$newpass);
@@ -74,6 +83,7 @@ try {
 
         $sth2->execute();
         $storeinfo=$sth2->fetchAll();
+    //once everything is executed, redirect to login
         header('location:signin.php');
       }
 
